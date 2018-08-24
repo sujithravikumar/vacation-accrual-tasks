9012b0d83954c4f8eb4de36fee2fafb53df238df
Sujith <sujithravikumar@gmail.com>
2018-08-23 23:28:35 -0700

    Implement Update and Insert logic for vacation data

--- vacation-accrual-tasks/ForecastVacationData.cs
+++ vacation-accrual-tasks/ForecastVacationData.cs
@@ -10,8 +10,6 @@ namespace vacation_accrual_tasks
     {
         public ForecastVacationData()
         {
-            Console.WriteLine("Hello from Forecast Vacation");
-
             List<User> userList = GetUsers();
 
             foreach (User user in userList)
@@ -25,32 +23,73 @@ namespace vacation_accrual_tasks
 
                 UserData userData = GetUserData(user.Id);
 
-                if (userData == null)
-                {
-                    Console.WriteLine("The User Data is empty!");
-                    return;
-                }
-
                 DateTime currentPayPeriodStartDate =
                     GetCurrentPayPeriodStartDate(userData.Pay_Cycle_Regular);
 
-                List<VacationData> vacationDataList =
-                    GetVacationData(user.Id,
+                List<VacationData> vacationDataList = GetVacationData(user.Id,
                                     currentPayPeriodStartDate.AddDays(-14));
 
-                if (vacationDataList == null || vacationDataList.Count == 0)
+                int numberOfRowsToInsert =
+                        userData.Period > vacationDataList.Count ?
+                                userData.Period - vacationDataList.Count : 0;
+
+                DateTime lastStartDate = vacationDataList.Last().Start_Date;
+                DateTime lastEndDate = vacationDataList.Last().End_Date;
+                decimal lastBalance = vacationDataList.Last().Balance;
+                decimal lastForfeit = vacationDataList.Last().Forefeit;
+
+                // Update
+                for (int i = 1; i < vacationDataList.Count; i++)
                 {
-                    Console.WriteLine("The Vacation Data is empty!");
-                    return;
+                    decimal balance = 
+                        vacationDataList[i-1].Balance +
+                        vacationDataList[i].Accrual -
+                        vacationDataList[i].Take;
+
+                    decimal forfeit = balance > userData.Max_Balance ?
+                                          balance - userData.Max_Balance : 0;
+
+                    if (balance != vacationDataList[i].Balance || 
+                       forfeit != vacationDataList[i].Forefeit)
+                    {
+                        UpdateVacationData(
+                            vacationDataList[i].Id,
+                            balance,
+                            forfeit
+                        );
+
+                        if (i == vacationDataList.Count - 1)
+                        {
+                            lastBalance = balance;
+                            lastForfeit = forfeit;
+                        }
+                    }
                 }
 
-                foreach (VacationData vd in vacationDataList)
+                // Insert
+                for (int i = 1; i <= numberOfRowsToInsert; i++)
                 {
-                    Console.WriteLine($"{vd.Id}\t{vd.User_Id}\t" +
-                                      $"{vd.Start_Date}\t{vd.End_Date}\t" +
-                                      $"{vd.Accrual}\t{vd.Take}\t" +
-                                      $"{vd.Balance}\t{vd.Forefeit}");
+                    decimal take = 0;
+                    lastBalance += userData.Accrual;
+                    lastStartDate = lastStartDate.AddDays(14);
+                    lastEndDate = lastEndDate.AddDays(14);
+
+                    if (lastBalance > userData.Max_Balance)
+                    {
+                        take = 8;
+                        lastBalance -= take;
+                        if (lastBalance > userData.Max_Balance)
+                        {
+                            lastForfeit += lastBalance - userData.Max_Balance;
+                            lastBalance = userData.Max_Balance;
+                        }
+                    }
+
+                    InsertVacationData(user.Id, lastStartDate,
+                                       lastEndDate, userData.Accrual,
+                                       take, lastBalance, lastForfeit);
                 }
+
             }
         }
 
@@ -77,9 +116,14 @@ namespace vacation_accrual_tasks
                 userDataList = conn.Query<UserData>(querySQL, new {userId}).ToList();
             }
 
-            if(userDataList == null || userDataList.Count == 0)
+            if (userDataList == null || userDataList.Count == 0)
             {
                 return null;
+            }
+
+            if (userDataList[0] == null)
+            {
+                Console.WriteLine("The User Data is empty!");
             }
             // user_id is unique on database so it should return only one row
             return userDataList[0];
@@ -120,12 +164,96 @@ namespace vacation_accrual_tasks
                             WHERE
                                 user_id = @userId
                                 AND start_date >= 
-                                        to_date(@startDate, 'YYYY-MM-DD')";                
+                                        TO_DATE(@startDate, 'YYYY-MM-DD')
+                            ORDER BY
+                                id";
                 vacationDataList = conn.Query<VacationData>(querySQL,
                     new {userId, 
                         startDate = startDate.ToString("yyyy-MM-dd")}).ToList();
+
+                if (vacationDataList == null || vacationDataList.Count == 0)
+                {
+                    Console.WriteLine("The Vacation Data is empty!");
+                }
             }
             return vacationDataList;
+        }
+
+        static void UpdateVacationData(int id, decimal balance, decimal forfeit)
+        {
+            using (var conn = Program.OpenConnection(Program._connStr))
+            {
+                var updateSQL = @"UPDATE
+                                        public.vacation_data
+                                    SET
+                                        balance = @balance,
+                                        forfeit = @forfeit
+                                    WHERE
+                                        id = @id";
+                var res = conn.Execute(updateSQL, new {id, balance, forfeit});
+                if (res == 0)
+                {
+                    Console.WriteLine(String.Format(
+                        "Update failed for {0}", id));
+                }
+                // FIXME temporary logic
+                else
+                {
+                    Console.WriteLine("Update success");
+                }
+            }
+        }
+
+        static void InsertVacationData(
+            int userId, DateTime startDate, DateTime endDate, decimal accrual, 
+            decimal take, decimal balance, decimal forfeit)
+        {
+            using (var conn = Program.OpenConnection(Program._connStr))
+            {
+                var insertSQL = @"INSERT INTO
+                                        public.vacation_data
+                                        (
+                                            id,
+                                            user_id,
+                                            start_date,
+                                            end_date,
+                                            accrual,
+                                            take,
+                                            balance,
+                                            forfeit
+                                        )
+                                    VALUES
+                                        (
+                                            nextval('vacation_data_id_seq'),
+                                            @userId,
+                                            TO_DATE(@startDate, 'YYYY-MM-DD'),
+                                            TO_DATE(@endDate, 'YYYY-MM-DD'),
+                                            @accrual,
+                                            @take,
+                                            @balance,
+                                            @forfeit
+                                        )";
+                var res = conn.Execute(insertSQL, 
+                   new {
+                        userId,
+                        startDate = startDate.ToString("yyyy-MM-dd"),
+                        endDate = endDate.ToString("yyyy-MM-dd"),
+                        accrual,
+                        take,
+                        balance,
+                        forfeit 
+                       }
+                );
+                if (res == 0)
+                {
+                    Console.WriteLine("Insert failed!");
+                }
+                // FIXME temporary logic
+                else
+                {
+                    Console.WriteLine("Insert success");
+                }
+            }
         }
     }
 }
