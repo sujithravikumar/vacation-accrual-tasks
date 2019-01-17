ecf6449e7055d614c66a0594beb311a673915f31
Sujith Ravi Kumar <sujithravikumar@gmail.com>
2019-01-16 22:45:03 -0800

    Add new class for user email data

---
+++ vacation-accrual-tasks/UserEmailData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace vacation_accrual_tasks
+{
+    public class UserEmailData
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public bool Start_Date_Even_Ww { get; set; }
+        public int Email_Alert_Days_Before { get; set; }
+    }
+}
